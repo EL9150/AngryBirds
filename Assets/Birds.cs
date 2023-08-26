905214b8189280f6c81e686621bb79da823616eb
elin34 <elin34@ucdc.edu>
2023-08-26 22:46:52 +0800

    Added: birds won't be able to be dragged by the player anymore after the first shot attempt

--- Assets/Birds.cs
+++ Assets/Birds.cs
@@ -10,27 +10,38 @@ public class Birds : MonoBehaviour
     public Vector2 slingshot_force; // the greater the force, the faster the bird gets when the player releases the mouse button
     public int bird_speed = 460;
     public string scene_name;
+    bool is_shot = false; // bird isn't shot yet
 
     // Player clicks on the bird.
     public void OnMouseDown() 
-    {
-        GetComponent<SpriteRenderer> ().color = Color.blue; // bird is highlighted in blue
+    {   
+        if (!is_shot)
+        {
+            GetComponent<SpriteRenderer> ().color = Color.blue; // bird is highlighted in blue
+        }
     }
 
     // Player releases the mouse button.
     public void OnMouseUp() 
     {
-        GetComponent<SpriteRenderer> ().color = Color.white; // bird's color is back to normal
-        GetComponent<Rigidbody2D> ().gravityScale = 1; // birds are affected by gravity after the player releases the mouse button
-        slingshot_force = init_position - transform.position; // dragging the bird to the left will shoot the bird to the right
-        GetComponent<Rigidbody2D> ().AddForce(slingshot_force * bird_speed); // shoot the bird
+        if (!is_shot)
+        {
+            GetComponent<SpriteRenderer> ().color = Color.white; // bird's color is back to normal
+            GetComponent<Rigidbody2D> ().gravityScale = 1; // birds are affected by gravity after the player releases the mouse button
+            slingshot_force = init_position - transform.position; // dragging the bird to the left will shoot the bird to the right
+            GetComponent<Rigidbody2D> ().AddForce(slingshot_force * bird_speed); // shoot the bird
+            is_shot = true;
+        }
     }
 
     // Drag the bird.
     public void OnMouseDrag() 
     {
-        Vector3 MouseDestination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(MouseDestination.x, MouseDestination.y);
+        if (!is_shot)
+        {
+            Vector3 MouseDestination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(MouseDestination.x, MouseDestination.y);
+        }
     }
 
     // initialize the bird's initial position
