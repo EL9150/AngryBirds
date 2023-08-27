9c06776801eac7a9788318785feb6cae4e15ca14
elin34 <elin34@ucdc.edu>
2023-08-27 21:20:24 +0800

    Added: There will be an explosion effect when a monster is destroyed.

--- Assets/Enemies.cs
+++ Assets/Enemies.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {   
-    // destroy the enemy when it gets hit by a bird, or hit by a crate from the top
+    public GameObject Destroyed_Effect; // an explosion effect that gets triggered when a monster die
+
+    // destroy the enemy when it gets hit by a bird, or by a crate from the top
     void OnCollisionEnter2D(Collision2D target) 
     {
         if (target.collider.GetComponent<Birds> ()) 
-        {
+        {   
+            // trigger the explosion effect at where the collision occurs (no rotation)
+            // then destroy the monster object
+            Instantiate(Destroyed_Effect, transform.position, Quaternion.identity);
             Destroy(gameObject); // hit by bird
         }
-        else if (target.GetContact(0).normal.y < 0) 
-        {
+        else if (target.GetContact(0).normal.y < 0.5) 
+        {   
+            Instantiate(Destroyed_Effect, transform.position, Quaternion.identity);
             Destroy(gameObject); // hit by crates from the top
         }
     }
